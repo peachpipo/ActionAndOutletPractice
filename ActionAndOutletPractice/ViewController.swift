6b9db14d1d4b48b24b888a111ad28e56d9807ae4
Lisa <peachpipo@gmail.com>
2021-01-11 20:07:48 +0800

    Initial Commit

---
+++ ActionAndOutletPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActionAndOutletPractice
+//
+//  Created by lijensu on 2021/1/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
